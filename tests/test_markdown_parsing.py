be6f85d8e94fb7f7799e99d9148d5d0bc4b5edc7
Rahul Krishna <i.m.ralk@gmail.com>
2024-06-25 16:58:05 -0400

    version 0.1.1. Patched to remove ipdb

--- tests/test_markdown_parsing.py
+++ tests/test_markdown_parsing.py
@@ -1,6 +1,5 @@
 import pytest
 
-from .conftest import MarkdownSamples
 from yank_code import extract_code_blocks
 
 
@@ -47,17 +46,6 @@ def test_extract_code_blocks_invalid(markdown_with_invalid_code_block):
 def test_extract_code_blocks_inline(markdown_with_inline_code):
     code_blocks = extract_code_blocks(markdown_with_inline_code)
     assert len(code_blocks) == 0
-
-
-@pytest.mark.parametrize(
-    "markdown_sample", [sample.value for sample in MarkdownSamples]
-)
-def test_extract_code_blocks_all_samples(markdown_sample):
-    code_blocks = extract_code_blocks(markdown_sample)
-    assert isinstance(code_blocks, list)
-    for block in code_blocks:
-        assert "code" in block
-        assert "language" in block
 
 
 def test_extract_code_blocks_empty_input():
